c1aed9692f7dbc428169dca08da5ad2717a44686
asus <20084040@qq.com>
2018-07-25 17:33:56 +0800

    .

--- gk/ws/web.go
+++ gk/ws/web.go
@@ -52,12 +52,16 @@ func (p *Param) Start(ps int64) int64 {
 }
 
 func SiteId(c *gin.Context) int64 {
-	idx := strings.Index(c.Request.Host, ".ecdiy.cn")
 	sql := "select SiteId from site.BindDomain where Domain=?"
 	host := c.Request.Host
+	ix := strings.Index(host, ":")
+	if ix > 0 {
+		host = host[0:ix]
+	}
+	idx := strings.Index(host, ".ecdiy.cn")
 	if idx > 0 {
 		sql = "select Id from site.Site where SubDomain=?"
-		host = c.Request.Host[0:idx]
+		host = host[0:idx]
 	}
 	hId, qb, _ := Gpa.QueryInt64(sql, host)
 	if qb {
