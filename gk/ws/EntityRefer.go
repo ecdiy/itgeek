424fa9ffa3b5decc9a62a0055f24675439d4757b
asus <20084040@qq.com>
2018-07-25 12:37:23 +0800

    .

--- gk/ws/EntityRefer.go
+++ gk/ws/EntityRefer.go
@@ -9,5 +9,5 @@ type DaoReferer struct {
 
 	Check func(SiteId, TopicId int64, Referer string) (int64, bool, error) `select count(*) from TopicReferer where SiteId=? and TopicId=? and Referer=?`
 
-	Up func(SiteId, TopicId int64, Referer string) (int64, bool, error) `update TopicReferer set Click=Click+1 ,ClickAll=ClickAll+1 where SiteId=? and TopicId=? and Referer=?`
+	Up func(SiteId, TopicId int64, Referer string) (int64, error) `update TopicReferer set Click=Click+1 ,ClickAll=ClickAll+1 where SiteId=? and TopicId=? and Referer=?`
 }
